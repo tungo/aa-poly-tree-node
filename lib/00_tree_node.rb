d02fb2474d5873a1465f6fb87106091be0189ef1
Tu Ngo <tungo8810@gmail.com>
2017-03-31 10:43:49 -0700

    Create polytreenode class

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
 
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(node)
+    @parent = node if node.is_a?(PolyTreeNode)
+    node.children  << self 
+  end
 end
